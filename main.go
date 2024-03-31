217e59f81ee5a3717a1c1ffa3d1a9c961ad8333e
James Chen <chenhao540@gmail.com>
2024-03-31 13:52:09 -0700

    update names

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("hello aaron")
+		return c.SendString("hello aaron, jake")
 	})
 
 	app.Get("/:name", func(c *fiber.Ctx) error {
